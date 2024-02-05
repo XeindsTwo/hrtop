787328cf6eb948dfe087e861889803a062bf3575
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-02-05 12:54:28 +0300

    adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,48 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
+const body = document.body;
+const menuBtn = document.querySelector('.menu-btn');
+const header = document.querySelector('.header__mobile');
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+menuBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  body.classList.toggle('body--active');
+  header.classList.toggle('active');
+  menuBtn.classList.toggle('active');
+  menuBtn.blur();
+});
+
+function scrollToTarget(targetId) {
+  const targetSection = document.querySelector(targetId);
+  if (targetSection) {
+    body.classList.remove('body--active');
+    header.classList.remove('active');
+    menuBtn.classList.remove('active');
+    setTimeout(() => {
+      const targetOffset = targetSection.offsetTop;
+      window.scrollTo({top: targetOffset, behavior: 'smooth'});
+    }, 400);
+  }
+}
+
+function handleAnchorClick(event) {
+  event.preventDefault();
+  const targetId = this.getAttribute('href');
+  scrollToTarget(targetId);
+}
+
+for (const anchor of anchors) {
+  anchor.addEventListener('click', handleAnchorClick);
+  anchor.addEventListener('touchstart', handleAnchorClick, {passive: true});
+}
+
+const menuLinks = document.querySelectorAll('.header__link');
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', handleAnchorClick);
+  menuLink.addEventListener('touchstart', handleAnchorClick, {passive: true});
+});
+
 Fancybox.bind("[data-fancybox]", {});
 
 const swiperCases = new Swiper(".cases__swiper", {
